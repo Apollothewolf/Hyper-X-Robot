9c0a9be52d6066b7b5fead696e0364674cf89b1b
ᏗᎮ๏ɭɭ๏「ᵀʰᵉ ˡᵒⁿᵉ ʷᵒˡᶠ」 <72794063+Apollothewolf@users.noreply.github.com>
2020-11-02 13:30:49 +0530

    Update panel_string.py

--- tg_bot/modules/helper_funcs/panel_string.py
+++ tg_bot/modules/helper_funcs/panel_string.py
@@ -1,13 +1,13 @@
 from tg_bot import dispatcher, INTRO_IMG
 
 if INTRO_IMG is None:
-    HYPER_X_IMG = "https://telegra.ph/file/e1e804f82a68529627555.jpg"
+    HYPER_X_IMG = "https://telegra.ph/file/745c8d58320f4ee5fe60b.jpg"
 else:
   HYPER_X_IMG = INTRO_IMG    
 SOURCE_STRING = """
-✯Im built-in python 3 to help and manage groups
-✯To clone me [𝘾𝙡𝙞𝙘𝙠 𝙝𝙚𝙧𝙚](https://github.com/santo-surviver/Hyper-X-Robot)
-✯For bot updates [𝙅𝙤𝙞𝙣 𝙃𝙚𝙧𝙚](https://t.me/Universal_Blazer)
+നോക്കി ഇരുന്നോ ഇപ്പോൾ കിട്ടും😅😅
+
+
 """
 
 HELP = """
@@ -18,7 +18,6 @@ the things I can help you with
  ➪ /start: start the bot
  ➪ /help: PM's you this message.
  ➪ /help <module name>: PM's you info about that module.
- ➪ /source: Information about my source.
  ➪ /settings:
   » in PM: will send you your settings for all supported modules.
   » in a group: will redirect you to pm, with all that chat's settings.
